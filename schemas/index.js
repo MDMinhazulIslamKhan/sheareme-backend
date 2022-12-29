667ff10b60944e678a34d6f63bf46c90d1421526
MDMinhazulIslamKhan <113751180+MDMinhazulIslamKhan@users.noreply.github.com>
2022-12-30 01:54:58 +0600

    create schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from "./user";
+import save from "./save";
+import postedBy from "./postedBy";
+import pin from "./pin";
+import comment from "./comment";
+
+export const schemaTypes = [user, pin, comment, save, postedBy]
